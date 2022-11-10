1a462f8b257ff5131da0b4e25ee1b4c678eef5b4
xxhoeckyxx <christopher.hoeck@MacBook-Pro.fritz.box>
2022-11-10 23:46:39 +0100

    Aufgabe weiter geschrieben

--- Teil A - C Programmierung/Praktikum/hoeckch_25.4.5.c
+++ Teil A - C Programmierung/Praktikum/hoeckch_25.4.5.c
@@ -2,7 +2,7 @@
  Description: Aufgabe 25.4.5 - Strings und char-Zeiger Teil1
      Project: Praktikum Informatik 2
       Author: hoeckch80619@th-nuernberg.de
-        Date: 9-November-2022
+        Date: 10-November-2022
 -------------------------------------------------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,16 +12,76 @@ Erstellen Sie ein Pragramm wortlen.c, das eine Häufigkeitsverteilung über die 
 einem Text ausgibt. Wendet man dieses Programm z.B.auf die Programmdatei wortlen.c selbst
 an: wortlen <wortlen.c
 */
-int main()
+
+void print_table(int leange, int anzahl)
 {
-    int leange = 0, 
-        anzahl = 10;
-    printf("Wortleange  |   Anzahl  |\n");
-    printf("------------+-----------|\n");
+    printf("Wortleange\t|\tAnzahl\t|\n");
+    printf("----------------+---------------|\n");
     do
     {
-        printf("          %d |         %d |\n", leange, anzahl);
-        anzahl--;
-    }while(anzahl > 0);
+        printf("\t%d\t|\t%d\t|\n", leange, anzahl);
+    } while (anzahl < 0);
+}
+
+void einlesen_text(FILE *fp, char *strings, int *stellen)
+{
+    int i = *stellen;
+    fp = fopen("../hoeckch_25.4.5.c", "r");
+    if(fp == NULL)
+    {
+        printf("Datei konnte nicht geoeffnet werden.\n");
+    }
+    else
+    {
+        // komplette Datei zeichenweise einlesen
+        while((strings[i] = fgetc(fp)) != EOF)
+        {
+            //printf("%c ", strings[i]); /* Debug   */
+            i++;
+            fflush(stdin);
+        }
+        fclose(fp);
+    }
+    *stellen = i;   /*  Debug   */
+}
+
+void wortleange_ermitteln(char *strings, int *leange)
+{
+    *leange = strlen(strings);
+}
+
+void string_teilen(char *strings, int *wortanzahl, int *leange)
+{
+    int i = *wortanzahl;
+    char *abschnitt = NULL;
+    char *neachsterAbschnitt = NULL;
+
+    abschnitt = strtok_r(strings, " ", &neachsterAbschnitt);
+
+    while(abschnitt != NULL)
+    {
+        abschnitt = strtok_r(NULL, " ", &neachsterAbschnitt);
+        i++;
+        wortleange_ermitteln(abschnitt, leange);
+    }
+    *wortanzahl = i;
+}
+
+int main()
+{
+    FILE *file;
+    char string[10000];
+    int leange = 0, 
+        anzahl = 0,
+        komplette_wortanzahl = 0,
+        buchstaben = 0;
+
+    einlesen_text(file, string, &buchstaben);
+    string_teilen(string, &komplette_wortanzahl, &leange);
+
+    printf("Zeichen im Text: %d\n", buchstaben); /*  Debug   */
+    printf("%s \n", string);  /*  Debug   */
+
+    print_table(leange, anzahl);
     return 0;
 }
